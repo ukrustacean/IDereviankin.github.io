a34547df37329d4ac63bb7b9afee73fd5324655d
Your Name <you@example.com>
2022-04-28 11:48:01 +0300

    Added ColiSphericus

---
+++ ColiSphericus/paramlist.js
@@ -0,0 +1,4 @@
+function toggleParams() {
+    const x = document.getElementById('paramList').style.display
+    document.getElementById('paramList').style.display = x == "none" ? "block" : "none" 
+}
